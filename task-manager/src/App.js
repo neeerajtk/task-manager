54d39cf7b394630b03a7b0cb63b384f83c08659a
neeerajtk <neerajtk560@gmail.com>
2020-09-16 23:46:33 +0530

    bootstrap added

---
+++ task-manager/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import bootstrap from 'bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="display-4">Hello</h2>
+    </div>
+  );
+}
+
+export default App;
